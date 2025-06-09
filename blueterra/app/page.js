cc9150f7bcc3142cd16591e24da8ffced1e8a414
richurichu <paulsonrichu@gmail.com>
2025-06-09 10:38:06 +0530

    cleaned project, installed tailwind

---
+++ blueterra/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className='  flex-center'>
+      <h1 className="text-3xl font-bold underline">
+        Home page
+      </h1>
+    </div>
+  );
+}
