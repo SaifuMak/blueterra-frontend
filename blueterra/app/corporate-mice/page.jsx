477077b81de26eb5fdfbdf6d691705000564a426
richurichu <paulsonrichu@gmail.com>
2025-07-15 12:56:52 +0530

    increased the size of button in corporate

--- blueterra/app/corporate-mice/page.jsx
+++ blueterra/app/corporate-mice/page.jsx
@@ -237,13 +237,12 @@ export default function Corporate() {
                         <div className=" w-full max-sm:px-2  absolute inset-0 text-center space-y-2 lg:space-y-4 2xl:space-y-7   h-full text-dark-28  flex flex-col justify-center items-center">
                             <p className=" vertically-animated-element md:text-2xl xl:text-3xl font-light  md:leading-10 w-11/12  md:w-9/12 xl:w-8/12 2xl:w-6/12 md:px-10  ">If you’re ready to bring beautiful, unforgettable events to life, contact us today</p>
                             <h3 className={`${playfair.className} font-medium vertically-animated-element max-sm:px-2  text-dark-4B text-xl md:text-4xl  xl:text-[50px]`}>Let’s start planning your perfect experience.</h3>
-                            <Button text='CONTACT US' buttonStyle={` transition-all vertically-animated-element duration-500 mt-3 md:mt-5 xl:mt-2  ease-in-out font-medium max-md:text-sm px-4 lg:px-6 xl:px-16 py-1.5 xl:py-2.5 `} />
-
+                            <Button text='CONTACT US' buttonStyle={` transition-all vertically-animated-element duration-500 mt-3 md:mt-5 xl:mt-2  ease-in-out font-medium max-sm:text-xs max-md:text-sm px-4 lg:px-6 xl:px-16 py-1.5 xl:py-2.5 `} />
                         </div>
                     </div>
 
 
-                    <div className=" w-full mt-16  overflow-hidden text-dark-28 flex flex-col items-center h-full relative">
+                    <div className=" w-full mt-16  overflow-hidden text-dark-28 flex flex-col items-center h-full relative ">
 
                         <Events firstTitle='Private Events '
                             secondTitle='& Occasions'
