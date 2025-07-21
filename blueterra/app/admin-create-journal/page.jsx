37df26896a2dc8125e249ba721133a6ce48f1910
richurichu <paulsonrichu@gmail.com>
2025-07-21 22:32:41 +0530

    journal creation

--- blueterra/app/admin-create-journal/page.jsx
+++ blueterra/app/admin-create-journal/page.jsx
@@ -73,6 +73,18 @@ export default function AdminJournals() {
         }, 500);
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent page reload
+        alert('Form submitted:');
+        // Example: send data to backend
+        // fetch('/api/submit', {
+        //   method: 'POST',
+        //   headers: { 'Content-Type': 'application/json' },
+        //   body: JSON.stringify(formData),
+        // });
+    };
+
+
 
     useEffect(() => {
         setIsClient(true);
@@ -104,122 +116,128 @@ export default function AdminJournals() {
                 <Sidebar />
 
                 <div className=" flex-1 text-dark-28  mt-10  ml-6  mr-8 mb-20   ">
-                    <div className=" w-full  h-full space-x-5  flex">
-                        <div className=" flex-1 flex-col space-y-5    ">
-                            <h1 className=" text-2xl 2xl:text-3xl font-normal text-dark-28 ">Add New Journal</h1>
-                            <div className=" w-full mt-10 space-y-5 ">
-                                <input type="text" value={formDataState.title} name="title" onChange={handleChange} placeholder="Enter title" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
-                                <input type="text" value={formDataState.slug} name="slug" onChange={handleChange} placeholder="Enter slug" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
+
+                    <form onSubmit={handleSubmit}>
+
+                        <div className=" w-full  h-full space-x-5  flex">
+
+                            <div className=" flex-1 flex-col space-y-5    ">
+                                <h1 className=" text-2xl 2xl:text-3xl font-normal text-dark-28 ">Add New Journal</h1>
+                                <div className=" w-full mt-10 space-y-5 ">
+                                    <input type="text" value={formDataState.title} name="title" onChange={handleChange} placeholder="Enter title" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " required />
+                                    <input type="text" value={formDataState.slug} name="slug" onChange={handleChange} placeholder="Enter slug" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " required />
+                                </div>
+
+                                <div className=" min-h-[400px]   xl:min-h-[300px] outline-none">
+                                    <Editor
+                                        onInit={(evt, editor) => editorRef.current = editor}
+                                        apiKey='5x0d43so5yodigr6a7p6b1a09jh9n0ugfks5wljq1r0lm2wm'
+                                        init={{
+                                            plugins: [
+                                                // Core editing features
+                                                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                                                // Your account includes a free trial of TinyMCE premium features
+                                                // Try the most popular premium features until Jul 30, 2025:
+                                                // 'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
+                                            ],
+
+                                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                                            tinycomments_mode: 'embedded',
+                                            tinycomments_author: 'Author name',
+                                            mergetags_list: [
+                                                { value: 'First.Name', title: 'First Name' },
+                                                { value: 'Email', title: 'Email' },
+                                            ],
+                                            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+                                        }}
+                                    // initialValue="Welcome to TinyMCE!"
+                                    />
+
+                                </div>
+
+                                <div className=" w-full  space-y-5 ">
+                                    <input type="text" value={formDataState.meta_title} name="meta_title" onChange={handleChange} placeholder="Enter Meta Title" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " required />
+                                    <input type="text" value={formDataState.meta_description} name="meta_description" onChange={handleChange} placeholder="Enter Meta Description" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " required />
+                                </div>
                             </div>
 
-                            <div className=" min-h-[400px]   xl:min-h-[300px] outline-none">
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    apiKey='5x0d43so5yodigr6a7p6b1a09jh9n0ugfks5wljq1r0lm2wm'
-                                    init={{
-                                        plugins: [
-                                            // Core editing features
-                                            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                                            // Your account includes a free trial of TinyMCE premium features
-                                            // Try the most popular premium features until Jul 30, 2025:
-                                            // 'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
-                                        ],
+                            <div className=" p-4 2xl:p-6 py-8  w-3/12 mt-10  h-full bg-white  border rounded-lg ">
 
-                                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                                        tinycomments_mode: 'embedded',
-                                        tinycomments_author: 'Author name',
-                                        mergetags_list: [
-                                            { value: 'First.Name', title: 'First Name' },
-                                            { value: 'Email', title: 'Email' },
-                                        ],
-                                        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-                                    }}
-                                // initialValue="Welcome to TinyMCE!"
-                                />
 
+                                <div className=" ">
+                                    <div className=" flex items-center justify-between ">
+                                        <h2 className=" text-lg 2xl:text-xl font-medium ">Post Journal </h2>
+
+                                        <button type="submit" className=" max-xl:hidden rounded-sm cursor-pointer text-sm font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
+
+                                    </div>
+                                    <button className=" xl:hidden mt-4 rounded-sm cursor-pointer text-sm w-full font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
+
+                                    <div className=" my-8 flex flex-col  ">
+                                        <p className=" max-xl:text-sm"> Save & Publish Later</p>
+                                        <button className=" mt-2  cursor-pointer  rounded-sm font-medium  border bg- px-4 py-2 text-sm bg-[#F7FBFD] text-dark-28">Save Draft</button>
+                                        {/* <p className=" ">save for later</p> */}
+
+                                    </div>
+                                </div>
+
+
+                                <div className="w-full 2xl:mt-10">
+                                    <label htmlFor="featuredImage" className="font-medium">
+                                        Featured Image
+                                    </label>
+
+                                    <input
+                                        type="file"
+                                        id="featuredImage"
+                                        accept="image/*"
+                                        ref={fileInputRef}
+                                        className="hidden"
+                                        onChange={handleImageSelect}
+                                    />
+
+                                    <div
+                                        onClick={handleDivClick}
+                                        className="mt-3 w-full cursor-pointer bg-[#F7FBFD] rounded-sm flex items-center justify-center h-28 xl:h-36 border relative overflow-hidden"
+                                    >
+                                        {previewImage ? (
+                                            <img
+                                                src={previewImage}
+                                                alt="Preview"
+                                                className="h-full object-cover w-full"
+                                            />
+                                        ) : (
+                                            <RiImageAddLine className="text-2xl text-slate-400" />
+                                        )}
+                                    </div>
+
+                                    <p
+                                        onClick={handleRemoveImage}
+                                        className={`text-right text-red-400 mt-2 text-sm hover:underline cursor-pointer ${previewImage ? 'opacity-100' : 'opacity-0'}`}
+                                    >
+                                        Remove Featured Image
+                                    </p>
+
+                                </div>
+
+                                <div className=" w-full  2xl:mt-10 ">
+                                    <div className=" flex max-xl:flex-col xl:items-center justify-between">
+                                        <label htmlFor="text" className=" font-medium" >Select Catagory</label>
+                                        <p onClick={handleOpenCreateCategoryPopup} className=" cursor-pointer flex items-center text-sky-blue-dark font-medium "><RiAddCircleLine className=" 2xl:text-xl mr-0.5 " /> Add</p>
+
+                                    </div>
+                                    <div className=" flex flex-col mt-3 space-y-3 max-h-96 overflow-y-auto ">
+                                        {sampleCategories?.map((category, index) => (
+                                            <div key={index} onClick={() => handleCategorySelection(category)} className=" flex items-center  text-sm transition-all duration-500 cursor-pointer"> <span className="  inline-block border-dark-28/30 2xl:size-4 size-3    shrink-0 border  mr-2 "> {formDataState.selectedCategory === category && <AiOutlineCheck className="text-sm text-black" />}</span>{category}</div>
+                                        ))}
+                                    </div>
+
+                                </div>
                             </div>
 
-                            <div className=" w-full  space-y-5 ">
-                                <input type="text" value={formDataState.meta_title} name="meta_title" onChange={handleChange} placeholder="Enter Meta Title" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
-                                <input type="text" value={formDataState.meta_description} name="meta_description" onChange={handleChange} placeholder="Enter Meta Description" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
-                            </div>
                         </div>
+                    </form>
 
-                        <div className=" p-4 2xl:p-6 py-8  w-3/12 mt-10  h-full bg-white  border rounded-lg ">
-
-
-                            <div className=" ">
-                                <div className=" flex items-center justify-between ">
-                                    <h2 className=" text-lg 2xl:text-xl font-medium ">Post Journal </h2>
-
-                                    <button className=" max-xl:hidden rounded-sm cursor-pointer text-sm font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
-
-                                </div>
-                                <button className=" xl:hidden mt-4 rounded-sm cursor-pointer text-sm w-full font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
-
-                                <div className=" my-8 flex flex-col  ">
-                                    <p className=" max-xl:text-sm"> Save & Publish Later</p>
-                                    <button className=" mt-2  cursor-pointer  rounded-sm font-medium  border bg- px-4 py-2 text-sm bg-[#F7FBFD] text-dark-28">Save Draft</button>
-                                    {/* <p className=" ">save for later</p> */}
-
-                                </div>
-                            </div>
-
-
-                            <div className="w-full 2xl:mt-10">
-                                <label htmlFor="featuredImage" className="font-medium">
-                                    Featured Image
-                                </label>
-
-                                <input
-                                    type="file"
-                                    id="featuredImage"
-                                    accept="image/*"
-                                    ref={fileInputRef}
-                                    className="hidden"
-                                    onChange={handleImageSelect}
-                                />
-
-                                <div
-                                    onClick={handleDivClick}
-                                    className="mt-3 w-full cursor-pointer bg-[#F7FBFD] rounded-sm flex items-center justify-center h-28 xl:h-36 border relative overflow-hidden"
-                                >
-                                    {previewImage ? (
-                                        <img
-                                            src={previewImage}
-                                            alt="Preview"
-                                            className="h-full object-cover w-full"
-                                        />
-                                    ) : (
-                                        <RiImageAddLine className="text-2xl text-slate-400" />
-                                    )}
-                                </div>
-
-                                <p
-                                    onClick={handleRemoveImage}
-                                    className={`text-right text-red-400 mt-2 text-sm hover:underline cursor-pointer ${previewImage ? 'opacity-100' : 'opacity-0'}`}
-                                >
-                                    Remove Featured Image
-                                </p>
-
-                            </div>
-
-                            <div className=" w-full  2xl:mt-10 ">
-                                <div className=" flex max-xl:flex-col xl:items-center justify-between">
-                                    <label htmlFor="text" className=" font-medium" >Select Catagory</label>
-                                    <p onClick={handleOpenCreateCategoryPopup} className=" cursor-pointer flex items-center text-sky-blue-dark font-medium "><RiAddCircleLine className=" 2xl:text-xl mr-0.5 " /> Add</p>
-
-                                </div>
-                                <div className=" flex flex-col mt-3 space-y-3 max-h-96 overflow-y-auto ">
-                                    {sampleCategories?.map((category, index) => (
-                                        <div key={index} onClick={() => handleCategorySelection(category)} className=" flex items-center  text-sm transition-all duration-500 cursor-pointer"> <span className="  inline-block border-dark-28/30 2xl:size-4 size-3    shrink-0 border  mr-2 "> {formDataState.selectedCategory === category && <AiOutlineCheck className="text-sm text-black" />}</span>{category}</div>
-                                    ))}
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
 
                 </div>
 
