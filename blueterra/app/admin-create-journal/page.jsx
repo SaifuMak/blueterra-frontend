ab25b59af1b1aa94a74ca85672a1aa33eac1d6d0
richurichu <paulsonrichu@gmail.com>
2025-07-21 17:57:02 +0530

    new commit

--- blueterra/app/admin-create-journal/page.jsx
+++ blueterra/app/admin-create-journal/page.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { useRouter } from 'next/navigation';
 import { Editor } from '@tinymce/tinymce-react';
 import { useRef, useState, useEffect } from 'react';
-import { RiImageAddLine, AiOutlineCheck, RiAddCircleLine } from '@/components/reactIcons'
+import { RiImageAddLine, AiOutlineCheck, RiAddCircleLine, RxCross2 } from '@/components/reactIcons'
 import { rubik } from '@/app/fonts'
 
 
@@ -16,7 +16,10 @@ export default function AdminJournals() {
 
     const [previewImage, setPreviewImage] = useState(null);
     const [journalCoverImage, setJournalCoverImage] = useState(null)
+
+    const [createCategoryPopupOpened, setCreateCategoryPopupOpened] = useState(false)
     const fileInputRef = useRef();
+    const createCategoryInputRef = useRef()
 
     const [sampleCategories, setSampleCategories] = useState(['Adventure', 'Beach', 'Cultural', ' Nature & Wildlife', 'Family Travel', 'Food & Culinary'])
 
@@ -63,10 +66,18 @@ export default function AdminJournals() {
         fileInputRef.current.value = null; // reset input
     };
 
+    const handleOpenCreateCategoryPopup = () => {
+        setCreateCategoryPopupOpened(true)
+        setTimeout(() => {
+            createCategoryInputRef?.current.focus()
+        }, 500);
+    }
+
 
     useEffect(() => {
         setIsClient(true);
     }, []);
+
 
     const handleGetContent = () => {
         if (editorRef.current) {
@@ -83,7 +94,7 @@ export default function AdminJournals() {
 
 
     return (
-        <div className={`${rubik.className} min-h-screen w-full`}>
+        <div className={`${rubik.className} relative min-h-screen w-full`}>
 
             <Navbar />
 
@@ -95,13 +106,13 @@ export default function AdminJournals() {
                 <div className=" flex-1 text-dark-28  mt-10  ml-6  mr-8 mb-20   ">
                     <div className=" w-full  h-full space-x-5  flex">
                         <div className=" flex-1 flex-col space-y-5    ">
-                            <h1 className=" text-3xl font-normal text-dark-28 ">Add New Journal</h1>
+                            <h1 className=" text-2xl 2xl:text-3xl font-normal text-dark-28 ">Add New Journal</h1>
                             <div className=" w-full mt-10 space-y-5 ">
                                 <input type="text" value={formDataState.title} name="title" onChange={handleChange} placeholder="Enter title" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
                                 <input type="text" value={formDataState.slug} name="slug" onChange={handleChange} placeholder="Enter slug" className="bg-white rounded-sm outline-none py-2 border w-full px-4  " />
                             </div>
 
-                            <div className="  min-h-[300px]">
+                            <div className=" min-h-[400px]   xl:min-h-[300px] outline-none">
                                 <Editor
                                     onInit={(evt, editor) => editorRef.current = editor}
                                     apiKey='5x0d43so5yodigr6a7p6b1a09jh9n0ugfks5wljq1r0lm2wm'
@@ -113,6 +124,7 @@ export default function AdminJournals() {
                                             // Try the most popular premium features until Jul 30, 2025:
                                             // 'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
                                         ],
+
                                         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
                                         tinycomments_mode: 'embedded',
                                         tinycomments_author: 'Author name',
@@ -133,32 +145,28 @@ export default function AdminJournals() {
                             </div>
                         </div>
 
-                        <div className=" p-6 py-8  w-3/12 mt-10  h-full bg-white  border rounded-lg ">
+                        <div className=" p-4 2xl:p-6 py-8  w-3/12 mt-10  h-full bg-white  border rounded-lg ">
 
 
                             <div className=" ">
                                 <div className=" flex items-center justify-between ">
-                                    <h2 className=" text-xl font-medium ">Post Journal </h2>
+                                    <h2 className=" text-lg 2xl:text-xl font-medium ">Post Journal </h2>
 
-                                    <button className=" rounded-sm cursor-pointer text-sm font-medium tracking-wide bg-sky-blue-dark px-6 py-1.5 text-white">Publish</button>
+                                    <button className=" max-xl:hidden rounded-sm cursor-pointer text-sm font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
 
                                 </div>
+                                <button className=" xl:hidden mt-4 rounded-sm cursor-pointer text-sm w-full font-medium tracking-wide bg-sky-blue-dark px-4 2xl:px-6 py-1.5 text-white">Publish</button>
+
                                 <div className=" my-8 flex flex-col  ">
-                                    <p className=""> Save & Publish Later</p>
-                                    <button className=" mt-2 cursor-pointer  rounded-sm font-medium  border bg- px-4 py-2 text-sm bg-[#F7FBFD] text-dark-28">Save Draft</button>
+                                    <p className=" max-xl:text-sm"> Save & Publish Later</p>
+                                    <button className=" mt-2  cursor-pointer  rounded-sm font-medium  border bg- px-4 py-2 text-sm bg-[#F7FBFD] text-dark-28">Save Draft</button>
                                     {/* <p className=" ">save for later</p> */}
 
                                 </div>
                             </div>
-                            {/* <div className=" w-full mt-10  ">
-                                <label htmlFor="text" className=" font-medium" >Featured Image</label>
-                                <div className=" mt-3 w-full cursor-pointer bg-[#F7FBFD] rounded-sm flex-center h-36 border ">
-                                    <RiImageAddLine className=" text-2xl text-slate-400  " />
-                                </div>
-                                <p className=" text-right text-red-400 mt-1 text-sm hover:underline cursor-pointer">Remove Featured Image</p>
-                            </div> */}
 
-                            <div className="w-full mt-10">
+
+                            <div className="w-full 2xl:mt-10">
                                 <label htmlFor="featuredImage" className="font-medium">
                                     Featured Image
                                 </label>
@@ -174,7 +182,7 @@ export default function AdminJournals() {
 
                                 <div
                                     onClick={handleDivClick}
-                                    className="mt-3 w-full cursor-pointer bg-[#F7FBFD] rounded-sm flex items-center justify-center h-36 border relative overflow-hidden"
+                                    className="mt-3 w-full cursor-pointer bg-[#F7FBFD] rounded-sm flex items-center justify-center h-28 xl:h-36 border relative overflow-hidden"
                                 >
                                     {previewImage ? (
                                         <img
@@ -187,7 +195,6 @@ export default function AdminJournals() {
                                     )}
                                 </div>
 
-
                                 <p
                                     onClick={handleRemoveImage}
                                     className={`text-right text-red-400 mt-2 text-sm hover:underline cursor-pointer ${previewImage ? 'opacity-100' : 'opacity-0'}`}
@@ -197,15 +204,15 @@ export default function AdminJournals() {
 
                             </div>
 
-                            <div className=" w-full  mt-10 ">
-                                <div className=" flex items-center justify-between">
+                            <div className=" w-full  2xl:mt-10 ">
+                                <div className=" flex max-xl:flex-col xl:items-center justify-between">
                                     <label htmlFor="text" className=" font-medium" >Select Catagory</label>
-                                    <p className=" cursor-pointer flex items-center text-sky-blue-dark font-medium "><RiAddCircleLine className=" text-xl mr-0.5 " /> Add</p>
+                                    <p onClick={handleOpenCreateCategoryPopup} className=" cursor-pointer flex items-center text-sky-blue-dark font-medium "><RiAddCircleLine className=" 2xl:text-xl mr-0.5 " /> Add</p>
 
                                 </div>
                                 <div className=" flex flex-col mt-3 space-y-3 max-h-96 overflow-y-auto ">
                                     {sampleCategories?.map((category, index) => (
-                                        <div key={index} onClick={() => handleCategorySelection(category)} className=" flex items-center  text-sm transition-all duration-500 cursor-pointer"> <span className="  inline-block border-dark-28/30 size-4    shrink-0 border  mr-2 "> {formDataState.selectedCategory === category && <AiOutlineCheck className="text-sm text-black" />}</span>{category}</div>
+                                        <div key={index} onClick={() => handleCategorySelection(category)} className=" flex items-center  text-sm transition-all duration-500 cursor-pointer"> <span className="  inline-block border-dark-28/30 2xl:size-4 size-3    shrink-0 border  mr-2 "> {formDataState.selectedCategory === category && <AiOutlineCheck className="text-sm text-black" />}</span>{category}</div>
                                     ))}
                                 </div>
 
@@ -214,10 +221,22 @@ export default function AdminJournals() {
 
                     </div>
 
-
                 </div>
 
             </div>
+
+
+
+            {createCategoryPopupOpened && <div className="fixed z-50 bg-white/70 text-dark-28 inset-0 flex items-center justify-center">
+                <div className="bg-white relative rounded-lg  shadow-xl p-6 w-100">
+                    <h2 className="text-lg font-medium mb-4 text-dark-4B ">Add New Category</h2>
+                    <input ref={createCategoryInputRef} type="text" placeholder="" className=" px-2 py-1 w-full  outline-none border rounded-sm" />
+                    <div className=" flex justify-center mt-4">
+                        <button className=" mt-1 cursor-pointer rounded-sm font-medium  border bg- px-4 py-1 text-sm bg-[#F7FBFD] ">Save </button>
+                    </div>
+                    <RxCross2 onClick={() => setCreateCategoryPopupOpened(false)} className=" text-dark-4B cursor-pointer absolute text-xl top-3 right-3" />
+                </div>
+            </div>}
         </div>
     )
 }
