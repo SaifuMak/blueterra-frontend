fc852581b214fb11110292fea9c04640c1b7b390
richurichu <paulsonrichu@gmail.com>
2025-07-23 16:06:20 +0530

    status toggle completed

--- blueterra/app/admin-journals/page.jsx
+++ blueterra/app/admin-journals/page.jsx
@@ -8,6 +8,7 @@ import AXIOS_INSTANCE from "@/lib/axios";
 import { useEffect, useState } from "react";
 import Pagination from "@/components/generalComponents/Pagination";
 import { getPageNumber, getTotalPagesCount } from "../utils/paginationHelpers";
+import { IoEyeOutline, IoEyeOffOutline } from '@/components/reactIcons'
 
 export default function AdminBlogs() {
 
@@ -27,11 +28,14 @@ export default function AdminBlogs() {
     const handleAddJournal = () => {
         router.push('/admin-create-journal')
     }
+    const [selectedJournalStatus, setSelectedJournalStatus] = useState('Published')
 
 
-    const fetchJournals = async (page = 1) => {
+
+    const fetchJournals = async (page = 1, status = 'Published') => {
+
         try {
-            const response = await AXIOS_INSTANCE.get(`journals/?page=${page}`)
+            const response = await AXIOS_INSTANCE.get(`journals/?page=${page}&status=${status}`)
             setJournals(response.data?.results)
             setCurrentPage(page)
             const nextpage = getPageNumber(response.data.next)
@@ -48,6 +52,12 @@ export default function AdminBlogs() {
         }
     }
 
+    const toggleJournalCategory = (status) => {
+        setSelectedJournalStatus(status)
+        fetchJournals(1, status)
+    }
+
+
     useEffect(() => {
 
 
@@ -57,7 +67,7 @@ export default function AdminBlogs() {
 
 
     return (
-        <div className=" h-screen w-full ">
+        <div className=" min-h-screen w-full ">
 
             <Navbar />
 
@@ -66,9 +76,24 @@ export default function AdminBlogs() {
                 {/* sidebar */}
                 <Sidebar />
 
-                <div className=" flex-1  ml-4  mr-8 mb-0 p-10 rounded-xl bg-[#F7FBFD] w-full flex justify-center  h-[97vh] z-50">
+                <div className=" flex-1  flex-col  ml-4  mr-8 mb-0 p-10 rounded-xl bg-[#F7FBFD] w-full flex  min-h-[97vh] z-50">
+                    <div className=" flex justify-between items-center">
+                        <h2 className=" text-4xl font-medium text-dark-4B  ">Journals </h2>
 
-                    <div className=" w-full overflow-hidden rounded-lg  border  h-fit">
+                        <div className=" flex items-center space-x-4 text-dark-4B ">
+
+                            <div onClick={() => toggleJournalCategory('Published')} className=" flex items-center space-x-1 font-medium cursor-pointer ">
+                                <div className={`${selectedJournalStatus === 'Published' ? 'bg-[#394C5D]' : 'bg-transparent'} size-4 rounded-full  border-dark-4B/80 border`} ></div>
+                                <p className="">Published</p>
+                            </div>
+                            <div onClick={() => toggleJournalCategory('Drafted')} className=" flex items-center space-x-1  font-medium cursor-pointer ">
+                                <div className={`${selectedJournalStatus === 'Drafted' ? 'bg-[#394C5D]' : 'bg-transparent'} size-4 rounded-full  border-dark-4B/80 border`} ></div>
+                                <p className="">Drafted</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className=" w-full overflow-hidden rounded-lg  border mt-5  h-fit">
                         <table className="w-full text-lg  rounded-3xl text-left text-gray-700">
                             <thead className="bg-[#394C5D] rounded-3xl text-white  ">
                                 <tr>
@@ -79,6 +104,7 @@ export default function AdminBlogs() {
                                     <th onClick={handleAddJournal} className="px-4 py-5 text-center font-normal"><button className=" bg-custom-sky-blue cursor-pointer text-white rounded-sm px-6 py-1">Add</button></th>
                                 </tr>
                             </thead>
+
                             <tbody className=" bg-white  border">
                                 {journals?.map((item, index) => (
                                     <tr key={index} className=" rounded-3xl">
@@ -89,30 +115,33 @@ export default function AdminBlogs() {
                                         <td className={rowStyle}>
                                             <div className=" flex justify-center space-x-10">
                                                 <img src="/Icons/edit-black.svg" alt="edit" className=" size-4  cursor-pointer " />
-                                                <img src="/Icons/hide.svg" alt="edit" className=" size-4 cursor-pointer " />
+                                                <div className="">
+                                                    {item.is_published ? <IoEyeOutline /> : <IoEyeOffOutline />}
+                                                </div>
                                             </div>
                                         </td>
+
                                         <td className={rowStyle}>
                                             <div className=" flex justify-center ">
-                                                published
+                                                {item.is_published ? "published" : "drafted"}
                                             </div>
-
                                         </td>
 
                                     </tr>
                                 ))}
-
                             </tbody>
                         </table>
-                        <Pagination
-                            prevPage={prevPage}
-                            nextPage={nextPage}
-                            function_to_call={fetchJournals}
-                            currentPage={currentPage}
-                            TotalPages={totalPages}
-                        />
+
                     </div>
 
+                    <Pagination
+                        prevPage={prevPage}
+                        nextPage={nextPage}
+                        function_to_call={fetchJournals}
+                        currentPage={currentPage}
+                        TotalPages={totalPages}
+                        queryParameter={selectedJournalStatus}
+                    />
                 </div>
 
             </div>
