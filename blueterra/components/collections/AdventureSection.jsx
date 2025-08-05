64e5f99ed4bfc09c9f73f20d3394bc9c23b83237
richurichu <paulsonrichu@gmail.com>
2025-08-05 21:47:20 +0530

    added green texture in collections

--- blueterra/components/collections/AdventureSection.jsx
+++ blueterra/components/collections/AdventureSection.jsx
@@ -23,7 +23,10 @@ export default function AdventureSection() {
                     </div>
                 </div>
 
-                <div className=" lg:w-1/2 h-1/2 lg:h-full w-full text-center max-lg:py-5 bg-dark-beige flex flex-col items-center justify-center space-y-5 lg:space-y-7 2xl:space-y-10 ">
+                <div className=" lg:w-1/2 h-1/2 lg:h-full w-full text-center max-lg:py-5  flex flex-col items-center justify-center space-y-5 lg:space-y-7 2xl:space-y-10  overflow-hidden bg-cover bg-center bg-no-repeat"
+                    style={{
+                        backgroundImage: "url('/images/our-story/yellow-banner.png')",
+                    }}>
                     <h1 className={`${playfair.className}  text-dark-4B max-lg:px-4 text-2xl  md:text-4xl  lg:text-4xl xl:text-[43px] 2xl:text-[50px]`} >Your Adventure Begins Here</h1>
                     <p className="  max-lg:px-5 md:text-lg xl:text-xl 2xl:text-2xl max-w-sm  xl:max-w-lg 2xl:max-w-2xl font-light">Unforgettable adventures, tailor-made just for you — let’s turn your travel dreams into reality.</p>
                     <Button text="START EXPLORING" buttonStyle=" px-10 py-2.5   lg:text-base" />
