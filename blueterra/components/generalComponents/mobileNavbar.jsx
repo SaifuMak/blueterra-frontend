abd519a7811a2d27700beca69d39499f39694e9b
richurichu <paulsonrichu@gmail.com>
2025-08-13 09:53:21 +0530

    mobile navbar changes

--- blueterra/components/generalComponents/mobileNavbar.jsx
+++ blueterra/components/generalComponents/mobileNavbar.jsx
@@ -8,12 +8,15 @@ import { useState } from "react";
 import { rubik } from '@/app/fonts'
 import Button from "./Button";
 import { usePathname } from "next/navigation";
+import ZohoFormModal from "../Forms/ZohoFormModal";
 
 
 export default function mobileNavbar({ MenuItems }) {
 
     const [isMenuOpened, setIsMenuOpened] = useState(false)
     const pathname = usePathname()
+
+  const [formOpen, setFormOpen] = useState(false);
 
 
 
@@ -23,19 +26,22 @@ export default function mobileNavbar({ MenuItems }) {
             <div className={` ${rubik.className} w-full flex flex-col relative   z-30 justify-center`}>
 
                 <div className="flex justify-between w-full  h-[50px] items-center pr-5 ">
+                    <Link href='/'>
 
-                    <div className="w-[140px] relative h-[35px]  shrink-0 ">
-                        <Image
-                            src="/images/general/logo.png"
-                            alt="logo"
-                            fill
-                            className="object-contain"
-                            quality={100}
-                        />
-                    </div>
+                        <div className="w-[140px] relative h-[35px]  shrink-0 ">
+                            <Image
+                                src="/images/general/logo.png"
+                                alt="logo"
+                                fill
+                                className="object-contain"
+                                quality={100}
+                            />
+                        </div>
+                    </Link>
+
 
                     <div className=" flex space-x-6 justify-center items-center ">
-                        <IoSearchOutline className="text-2xl text-brand-blue font-semibold" />
+                        {/* <IoSearchOutline className="text-2xl text-brand-blue font-semibold" /> */}
                         <HiMenuAlt3 onClick={() => setIsMenuOpened(!isMenuOpened)} className="text-3xl text-brand-blue cursor-pointer " />
                     </div>
 
@@ -58,16 +64,16 @@ export default function mobileNavbar({ MenuItems }) {
                             <RxCross2 onClick={() => setIsMenuOpened(false)} className="text-3xl text-sky-blue-1 cursor-pointer " />
                         </div>
 
-                        <div className=" w-full h-[40px] flex border-b border-sky-blue-1/50  mt-5 space-x-1 items-center ">
+                        {/* <div className=" w-full h-[40px] flex border-b border-sky-blue-1/50  mt-5 space-x-1 items-center ">
                             <input type="text" placeholder="Search" className=" placeholder:text-base px-2 w-full h-full caret-text-sky-blue-1  outline-none" />
                             <IoSearchOutline className="text-2xl  text-sky-blue-1 font-semibold" />
-                        </div>
+                        </div> */}
 
                         <div className=" flex flex-col space-y-5 mt-5 font-extralight ">
                             {MenuItems.map((items, index) => (
                                 <Link key={index} href={items.link} className={`${items.url === pathname ? ' text-brand-blue' : ' text-dark-28'}`}>{items.nav}</Link>
                             ))}
-                            <Button text='PLAN YOUR EXPERIENCE' buttonStyle='text-nowrap max-md:text-[12px] px-4 lg:px-2 xl:px-6 py-1.5 lg:py-2 ' isHoverWhiteApplied={false} />
+                            <Button text='PLAN YOUR EXPERIENCE' buttonStyle='text-nowrap max-md:text-[12px] px-4 lg:px-2 xl:px-6 py-1.5 lg:py-2 ' isHoverWhiteApplied={false}  onClickFunction={() => setFormOpen(true)} />
 
                             {/* <Link href="/" className="   px-4 rounded-sm   bg-sky-blue-1  text-white w-fit py-1">Contact us</Link> */}
 
@@ -76,6 +82,8 @@ export default function mobileNavbar({ MenuItems }) {
                 </div>
 
             </div>
+                  <ZohoFormModal isOpen={formOpen} onClose={() => setFormOpen(false)} />
+            
         </>
     )
 }
