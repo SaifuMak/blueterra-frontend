c71f45faa2e94bd63ea4115c626e44dbe92f2bb5
richurichu <paulsonrichu@gmail.com>
2025-09-04 11:16:01 +0530

    r2 url updated

--- blueterra/next.config.mjs
+++ blueterra/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.pexels.com', 'pub-c75e3733f0bd4a078b015afdd3afc354.r2.dev'],
+        domains: ['images.pexels.com', 'pub-c75e3733f0bd4a078b015afdd3afc354.r2.dev', 'pub-02c3b688fac04f9881f9a14161aa2972.r2.dev'],
     },
 };
 
