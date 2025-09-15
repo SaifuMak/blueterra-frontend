53189c8d30ebbecb443727751b0ea71047aa2bae
richurichu <paulsonrichu@gmail.com>
2025-09-15 16:02:31 +0530

    english language for itinerary map

--- blueterra/components/ItineraryView/PlannedActivities/MapDemo.jsx
+++ blueterra/components/ItineraryView/PlannedActivities/MapDemo.jsx
@@ -192,8 +192,11 @@ export default function MapDemo({ expandCards, itineraryData }) {
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                       url="https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                 />
+
+                
 
                 {/* Draw routes */}
                 {routes.map((r, idx) => (
