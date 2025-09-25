6b482772514d8cc357fd96b89e48b8a8a13b4e39
richurichu <paulsonrichu@gmail.com>
2025-09-25 15:42:57 +0530

    seperated the scalling logic

--- blueterra/components/AnimatedVerticalCards/MobileAnimatedVerticalCard.jsx
+++ blueterra/components/AnimatedVerticalCards/MobileAnimatedVerticalCard.jsx
@@ -3,34 +3,12 @@ import { playfair } from '@/app/fonts'
 import { useRef } from "react"
 import { useState, useEffect } from "react"
 import gsap from 'gsap';
-
+import MobileAnimatedVerticalIndividualCard from "./MobileAnimatedVerticalIndividualCard";
 
 export default function MobileAnimatedVerticalCard({ page, CardData, selectedVerticalTileMobile, setSelectedVerticalTileMobile, handleSetCollectionRequestedToShowInMobile }) {
 
   const mobileVerticalTilesRef = useRef([])
   const [touchedCard, setTouchedCard] = useState(null)
-
-  const handleTouchStart = (index, e) => {
-    if (selectedVerticalTileMobile === index) return;
-    // setTouchedCard(index)
-    gsap.to(mobileVerticalTilesRef[index], {
-
-      flex: 2,
-      duration: 0.7,
-      ease: 'Power3.out'
-    })
-
-  };
-
-  const handleTouchEnd = (index, e) => {
-    if (selectedVerticalTileMobile === index) return;
-    gsap.to(mobileVerticalTilesRef[index], {
-      flex: 1,
-      duration: 0.7,
-      ease: 'Power3.out'
-    })
-
-  };
 
   const handleCardClick = (index) => {
     setSelectedVerticalTileMobile(index)
@@ -41,97 +19,21 @@ export default function MobileAnimatedVerticalCard({ page, CardData, selectedVer
     // });
   }
 
-  useEffect(() => {
-    if (!mobileVerticalTilesRef[selectedVerticalTileMobile]) {
-      return
-    }
-
-    gsap.to(mobileVerticalTilesRef[selectedVerticalTileMobile], {
-      flex: 9,
-      duration: 0.7,
-      ease: 'Power3.out'
-    });
-
-  }, [selectedVerticalTileMobile])
-
-
+  
 
 
   return (
     <div className="w-full min-h-[92vh] mt-[50px] overflow-hidden  flex flex-col ">
       {CardData?.map((card, index) => (
-        <div key={index}
-          ref={(e) => (mobileVerticalTilesRef[index] = e)}
-          onClick={() => handleCardClick(index)}
-          onTouchStart={(e) => handleTouchStart(index, e)}
-          onTouchEnd={(e) => handleTouchEnd(index, e)}
-          className={` flex-1 relative `}>
+        <MobileAnimatedVerticalIndividualCard key={index}
+          index={index}
+          card={card}
+          selectedVerticalTileMobile={selectedVerticalTileMobile}
+          setSelectedVerticalTileMobile={setSelectedVerticalTileMobile}
+          handleCardClick={handleCardClick}
+          page={page}
+          handleSetCollectionRequestedToShowInMobile={handleSetCollectionRequestedToShowInMobile} />
 
-          {/* <div key={index} ref={(e) => (mobileVerticalTilesRef[index] = e)} onClick={() => setSelectedVerticalTileMobile(index)} className={`  transition-all duration-500 ease-in-out relative ${selectedVerticalTileMobile === index ? 'flex-9' : 'flex-1'} `}> */}
-
-          <Image
-            src={card?.banner_image_public_url}
-            alt={card?.title}
-            fill
-            priority
-            style={{ objectFit: 'cover', objectPosition: 'center' }}
-          />
-
-
-          <div className={`absolute ${selectedVerticalTileMobile === index ? "opacity-0" : " opacity-100"} text-white transition-all duration-500 ease-in-out inset-0   bg-[#00284626]`}>
-            <div className="flex   w-full h-full px-3 mt-2 space-x-2 ">
-              <img src={card?.icon_public_url} alt={card?.title} className=" object-cover size-[27px]" />
-              <div className="bg-white/40 h-0.5 flex-1 my-3"></div>
-              <p className=''> {card?.title}</p>
-
-            </div>
-          </div>
-
-          <div className={`bg-[#104F82D9]/50 text-white transition-all duration-500 ease-in-out absolute inset-0 ${selectedVerticalTileMobile === index ? "opacity-100" : " opacity-0"}`}>
-            <div className=" w-full h-full p-6 ">
-
-              <p className='  flex  text-white text-lg  '>
-                <img src={card?.icon_public_url} alt={card.title} className=" object-cover size-[25px]" />
-                <span className={`font-normal text-xl ml-3 ${playfair.className}`}>{card.title}</span></p>
-
-              <hr className=' opacity-40 mt-1'></hr>
-              <h3 className={`font-normal mt-5 ${playfair.className}`}>Popular Journeys</h3>
-              {page === 'collections' && (
-                <div className="flex   w-11/12 flex-wrap  gap-y-1  gap-x-4 pr-3 font-light text-[12px] mt-2">
-                  {card?.popular_journeys
-                    ?.split(",")                // split by comma
-                    .map((item, index) => (
-                      <div key={index} className="flex">
-                        <span className="text-nowrap">{item.trim()}</span>
-                      </div>
-                    ))}
-                </div>
-              )}
-
-              {page === 'destinations' && <div className="flex   w-11/12 flex-wrap  gap-y-1  gap-x-4 pr-3 font-light text-[12px] mt-2">
-                {card?.countries?.map((country, index) => (
-                  <div key={index} className="  flex   ">
-                    <span className="text-nowrap">{country?.title}</span></div>
-                ))}
-              </div>}
-
-              <p className=" text-sm font-light mt-2 ">{card?.description}</p>
-
-              {page === 'collections' && <div className="space-y-1 font-light text-sm mt-3 h-[19vh]  gap-x-3 overflow-y-auto  flex flex-col ">
-                {card?.categories?.map((category, index) => (
-                  <div key={index} className=" text-white flex items-center text-sm">  <img src="/Icons/dot.svg" alt="dot" className="w-2.5 h-2.5" /> <p className=" ml-3">{category?.title}</p></div>
-                ))}
-              </div>}
-
-              <div className=" w-full flex mt-3 text-[12px] items-center justify-between">
-                <button onClick={() => handleSetCollectionRequestedToShowInMobile(index)} className="hover:bg-white/15 hover:ring-2 ring-white/80  bg-sky-blue-1 px-6 py-1.5 transition-all duration-700 ease-in-out  cursor-pointer  text-[12px]  rounded-sm  text-white w-fit h-fit">VIEW ALL</button>
-                {/* <button className="hover:bg-white/15 hover:ring-2 ring-white/80 flex items-center  bg-sky-blue-1 px-4 py-1.5 transition-all duration-700 ease-in-out  cursor-pointer  text-[12px] font-normal rounded-sm  text-white"><span className="mr-1"><img src='/Icons/filter.svg' className='size-4 '></img></span>Show Filters</button> */}
-              </div>
-
-            </div>
-          </div>
-
-        </div>
       ))}
     </div>
   )
---
+++ blueterra/components/AnimatedVerticalCards/MobileAnimatedVerticalIndividualCard.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import { useState, useRef, useEffect } from 'react'
+import Image from "next/image"
+import { playfair } from '@/app/fonts'
+import gsap from 'gsap';
+
+
+function MobileAnimatedVerticalIndividualCard({ index,
+    card,
+    selectedVerticalTileMobile,
+    setSelectedVerticalTileMobile,
+    handleCardClick,
+    page,
+    handleSetCollectionRequestedToShowInMobile }) {
+
+    const cardRef = useRef(null)
+
+    const handleTouchStart = (indexToAnimate, e) => {
+        if (selectedVerticalTileMobile === indexToAnimate) return;
+        gsap.to(cardRef.current, {
+            flex: 2,
+            duration: 0.7,
+            ease: 'Power3.out'
+        })
+
+    };
+
+    const handleTouchEnd = (indexToAnimate, e) => {
+        if (selectedVerticalTileMobile === indexToAnimate) return;
+        gsap.to(cardRef.current, {
+            flex: 1,
+            duration: 0.7,
+            ease: 'Power3.out'
+        })
+    };
+
+    useEffect(() => {
+        if (cardRef.current) {
+            gsap.to(cardRef.current, {
+                flex: selectedVerticalTileMobile === index ? 9 : 1,
+                duration: 0.7,
+                ease: 'Power3.out'
+            });
+        }
+    }, [selectedVerticalTileMobile])
+
+
+
+    return (
+        <div
+            ref={cardRef}
+            onClick={() => handleCardClick(index)}
+            onTouchStart={(e) => handleTouchStart(index, e)}
+            onTouchEnd={(e) => handleTouchEnd(index, e)}
+            className={` flex-1 relative `}>
+
+            {/* <div key={index} ref={(e) => (mobileVerticalTilesRef[index] = e)} onClick={() => setSelectedVerticalTileMobile(index)} className={`  transition-all duration-500 ease-in-out relative ${selectedVerticalTileMobile === index ? 'flex-9' : 'flex-1'} `}> */}
+
+            <Image
+                src={card?.banner_image_public_url}
+                alt={card?.title}
+                fill
+                priority
+                style={{ objectFit: 'cover', objectPosition: 'center' }}
+            />
+
+
+            <div className={`absolute ${selectedVerticalTileMobile === index ? "opacity-0" : " opacity-100"} text-white transition-all duration-500 ease-in-out inset-0   bg-[#00284626]`}>
+                <div className="flex   w-full h-full px-3 mt-2 space-x-2 ">
+                    <img src={card?.icon_public_url} alt={card?.title} className=" object-cover size-[27px]" />
+                    <div className="bg-white/40 h-0.5 flex-1 my-3"></div>
+                    <p className=''> {card?.title}</p>
+
+                </div>
+            </div>
+
+            <div className={`bg-[#104F82D9]/50 text-white transition-all duration-500 ease-in-out absolute inset-0 ${selectedVerticalTileMobile === index ? "opacity-100" : " opacity-0"}`}>
+                <div className=" w-full h-full p-6 ">
+
+                    <p className='  flex  text-white text-lg  '>
+                        <img src={card?.icon_public_url} alt={card.title} className=" object-cover size-[25px]" />
+                        <span className={`font-normal text-xl ml-3 ${playfair.className}`}>{card.title}</span></p>
+
+                    <hr className=' opacity-40 mt-1'></hr>
+                    <h3 className={`font-normal mt-5 ${playfair.className}`}>Popular Journeys</h3>
+                    {page === 'collections' && (
+                        <div className="flex   w-11/12 flex-wrap  gap-y-1  gap-x-4 pr-3 font-light text-[12px] mt-2">
+                            {card?.popular_journeys
+                                ?.split(",")                // split by comma
+                                .map((item, index) => (
+                                    <div key={index} className="flex">
+                                        <span className="text-nowrap">{item.trim()}</span>
+                                    </div>
+                                ))}
+                        </div>
+                    )}
+
+                    {page === 'destinations' && <div className="flex   w-11/12 flex-wrap  gap-y-1  gap-x-4 pr-3 font-light text-[12px] mt-2">
+                        {card?.countries?.map((country, index) => (
+                            <div key={index} className="  flex   ">
+                                <span className="text-nowrap">{country?.title}</span></div>
+                        ))}
+                    </div>}
+
+                    <p className=" text-sm font-light mt-2 ">{card?.description}</p>
+
+                    {page === 'collections' && <div className="space-y-1 font-light text-sm mt-3 h-[19vh]  gap-x-3 overflow-y-auto  flex flex-col ">
+                        {card?.categories?.map((category, index) => (
+                            <div key={index} className=" text-white flex items-center text-sm">  <img src="/Icons/dot.svg" alt="dot" className="w-2.5 h-2.5" /> <p className=" ml-3">{category?.title}</p></div>
+                        ))}
+                    </div>}
+
+                    <div className=" w-full flex mt-3 text-[12px] items-center justify-between">
+                        <button onClick={() => handleSetCollectionRequestedToShowInMobile(index)} className="hover:bg-white/15 hover:ring-2 ring-white/80  bg-sky-blue-1 px-6 py-1.5 transition-all duration-700 ease-in-out  cursor-pointer  text-[12px]  rounded-sm  text-white w-fit h-fit">VIEW ALL</button>
+                        {/* <button className="hover:bg-white/15 hover:ring-2 ring-white/80 flex items-center  bg-sky-blue-1 px-4 py-1.5 transition-all duration-700 ease-in-out  cursor-pointer  text-[12px] font-normal rounded-sm  text-white"><span className="mr-1"><img src='/Icons/filter.svg' className='size-4 '></img></span>Show Filters</button> */}
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MobileAnimatedVerticalIndividualCard
