de1693cd473a7b15a32f7c540ef5e5729c404d82
richurichu <paulsonrichu@gmail.com>
2025-09-25 15:59:01 +0530

    added meta details in cruise page

--- blueterra/app/cruise/page.jsx
+++ blueterra/app/cruise/page.jsx
@@ -1,5 +1,11 @@
 import CruiseClient from "@/components/clients/CruiseClient"
 
+export const metadata = {
+  title: "Cruise Packages & Exclusive Offers | Blueterra Cruises",
+  description:
+    "Explore Blueterra’s wide range of cruise packages and special offers. From luxury getaways to family adventures, discover tailored cruise experiences designed to deliver comfort, entertainment, and unforgettable journeys at sea.",
+};
+
 
 export default function cruise() {
     return (
