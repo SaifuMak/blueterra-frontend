5b9ae95266acdb0f6f51742208227c8973d9d158
richurichu <paulsonrichu@gmail.com>
2025-10-06 10:52:37 +0530

    change in marque

--- blueterra/components/Home/TestimonialsForMobile.jsx
+++ blueterra/components/Home/TestimonialsForMobile.jsx
@@ -14,7 +14,7 @@ function TestimonialsForMobile() {
                     <h2 className={`${playfair.className}   text-center heading-text max-sm:px-2`}>Trusted By Customers</h2>
                     <p className=" text-center mt-3  font-light max-sm:px-5">Experiences Shared by Our Travelers</p>
                 </div>
-                <Marquee pauseOnHover speed={60}>
+                <Marquee pauseOnHover pauseOnClick={false} speed={60}>
                     {testimonials?.map((testimonial, index) => (
                         <div key={index} className=" z-20  mx-5  my-4  py-10 px-5 bg-white  h-fit w-[300px]     rounded-2xl " style={{ boxShadow: '0 0 25px 1px rgba(153, 189, 188, 0.3)', }} >
                             <p className=" font-light md:leading-8 leading-7  ">{testimonial.message}</p>
