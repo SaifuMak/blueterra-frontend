143ad9418df7f2a70d51ad5117557d3532db9f8d
richurichu <paulsonrichu@gmail.com>
2025-10-06 10:58:50 +0530

    new commit

--- blueterra/components/Home/TestimonialsForMobile.jsx
+++ blueterra/components/Home/TestimonialsForMobile.jsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import Marquee from "react-fast-marquee";
 import { testimonials } from '@/constants/testimonials';
-import {playfair} from '@/app/fonts'
+import { playfair } from '@/app/fonts'
 import useGsapFadeIn from '@/app/hooks/Gsap/useGsapFadeIn';
+import { useState } from 'react';
 
 function TestimonialsForMobile() {
-     const testimonialTitleRef = useGsapFadeIn()
+    const testimonialTitleRef = useGsapFadeIn()
+    const [isHovered, setIsHovered] = useState(false)
+
     return (
         <div className=" flex   bg-sky-blue-light py-10 ">
             <div className=" w-11/12  mx-auto rounded-2xl  bg-white pt-10 pb-2">
@@ -14,9 +17,11 @@ function TestimonialsForMobile() {
                     <h2 className={`${playfair.className}   text-center heading-text max-sm:px-2`}>Trusted By Customers</h2>
                     <p className=" text-center mt-3  font-light max-sm:px-5">Experiences Shared by Our Travelers</p>
                 </div>
-                <Marquee pauseOnHover pauseOnClick={false} speed={60}>
+                <Marquee pauseOnClick={true} speed={60}>
                     {testimonials?.map((testimonial, index) => (
-                        <div key={index} className=" z-20  mx-5  my-4  py-10 px-5 bg-white  h-fit w-[300px]     rounded-2xl " style={{ boxShadow: '0 0 25px 1px rgba(153, 189, 188, 0.3)', }} >
+                        <div key={index} className=" z-20  mx-5  my-4  py-10 px-5 bg-white  h-fit w-[300px]     rounded-2xl " style={{ boxShadow: '0 0 25px 1px rgba(153, 189, 188, 0.3)', }}
+                            onTouchStart={() => setIsHovered(true)}   // finger touches
+                            onTouchEnd={() => setIsHovered(false)}   >
                             <p className=" font-light md:leading-8 leading-7  ">{testimonial.message}</p>
                             <p className=" text-sky-blue-dark mt-5">{testimonial.name}</p>
                             <p className=" font-light mt-2">{testimonial.country}</p>
