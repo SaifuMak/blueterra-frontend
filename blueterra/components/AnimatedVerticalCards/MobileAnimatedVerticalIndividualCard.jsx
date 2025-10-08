4b7281c6bf06070df13ede43089dc8a0e82839ed
richurichu <paulsonrichu@gmail.com>
2025-10-08 17:44:57 +0530

    new commit

--- blueterra/components/AnimatedVerticalCards/MobileAnimatedVerticalIndividualCard.jsx
+++ blueterra/components/AnimatedVerticalCards/MobileAnimatedVerticalIndividualCard.jsx
@@ -112,7 +112,7 @@ function MobileAnimatedVerticalIndividualCard({ index,
 
                     <p className=" text-sm font-light mt-2 ">{card?.description}</p>
 
-                    {page === 'collections' && <div className="space-y-1 font-light text-sm mt-3 h-[19vh]  gap-x-3 overflow-y-auto  flex flex-col ">
+                    {page === 'collections' && <div className="space-y-1 font-light text-sm mt-3 h-[15vh]  gap-x-3 overflow-y-auto  flex flex-col ">
                         {card?.categories?.map((category, index) => (
                             <div key={index} className=" text-white flex items-center text-sm">  <img src="/Icons/dot.svg" alt="dot" className="w-2.5 h-2.5" /> <p className=" ml-3">{category?.title}</p></div>
                         ))}
