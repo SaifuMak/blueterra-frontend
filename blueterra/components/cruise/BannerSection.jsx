fc2f99ea5d8f947b42c48a82b28313489dc37747
richurichu <paulsonrichu@gmail.com>
2025-10-22 17:14:17 +0530

    cruise widget in mobile

--- blueterra/components/cruise/BannerSection.jsx
+++ blueterra/components/cruise/BannerSection.jsx
@@ -40,7 +40,7 @@ function BannerSection({isMobile}) {
             ></video>
 
 
-            <div className=" w-full h-full absolute bg-black/10  inset-0 flex-center flex-col text-white ">
+            <div className=" w-full h-auto absolute bg-black/10  inset-0 flex-center flex-col text-white ">
                 <div className="flex-center flex-col space-y-5 lg:space-y-8 mt-10 overflow-x-hidden ">
                     <h1 className={` ${playfair.className}  ${isBannerVideoLoaded ? 'opacity-100 translate-y-0' : ' translate-y-5 opacity-0'} translate-all duration-700 ease-in-out text-3xl  max-md:px-5 text-center md:text-4xl lg:text-[60px] xl:text-[70px] 2xl:text-[80px] font-semibold `}>BlueTerra Cruise</h1>
                     <div className=" bg-white max-w-11/12  max-lg:max-h-[80vh] overflow-y-auto lg:max-w-10/12 sm:p-2 rounded-2xl "  {...( isMobile ? { 'data-lenis-prevent': true } : {})}>
