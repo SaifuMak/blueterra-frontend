93e54e2ad4d8c4e4a3ca616ad777720d29f14cba
richurichu <paulsonrichu@gmail.com>
2025-10-28 13:11:06 +0530

    change in overflow crusie widget

--- blueterra/components/cruise/BannerSection.jsx
+++ blueterra/components/cruise/BannerSection.jsx
@@ -5,7 +5,7 @@ import LoaderIcon from '../generalComponents/LoaderIcon';
 import { useState, useEffect, useRef } from 'react';
 import CruiseWidget from './CruiseWidget';
 
-function BannerSection({isMobile}) {
+function BannerSection({ isMobile }) {
 
     const [isBannerVideoLoaded, setIsBannerVideoLoaded] = useState(false)
 
@@ -41,16 +41,16 @@ function BannerSection({isMobile}) {
 
 
             <div className=" w-full h-auto absolute bg-black/10  inset-0 flex-center flex-col text-white ">
-                <div className="flex-center flex-col space-y-5 lg:space-y-8 mt-10 overflow-x-hidden ">
+                <div className="flex-center flex-col space-y-5 lg:space-y-8 mt-10  ">
                     <h1 className={` ${playfair.className}  ${isBannerVideoLoaded ? 'opacity-100 translate-y-0' : ' translate-y-5 opacity-0'} translate-all duration-700 ease-in-out text-3xl  max-md:px-5 text-center md:text-4xl lg:text-[60px] xl:text-[70px] 2xl:text-[80px] font-semibold `}>BlueTerra Cruise</h1>
-                    <div className=" max-w-11/12   border no-scroll-bar   max-lg:max-h-[80vh]  max-lg:h-full overflow-y-auto lg:max-w-10/12 sm:p-2 rounded-2xl "
-                      style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }} data-lenis-prevent >
+                    <div className=" max-w-11/12   border no-scroll-bar   max-lg:max-h-[80vh]  max-lg:h-full  max-lg:overflow-y-auto lg:max-w-10/12 sm:p-2 rounded-2xl "
+                        style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }} data-lenis-prevent >
                         <CruiseWidget />
                     </div>
                 </div>
             </div>
         </div>
-//  {...( isMobile ? { 'data-lenis-prevent': true } : {})}
+        //  {...( isMobile ? { 'data-lenis-prevent': true } : {})}
     )
 }
 
