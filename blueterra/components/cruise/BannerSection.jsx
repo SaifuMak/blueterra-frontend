8ae8dbae0e703681e972c8b62a3e14613fd8b599
richurichu <paulsonrichu@gmail.com>
2025-10-28 13:04:52 +0530

    scroll implemented

--- blueterra/components/cruise/BannerSection.jsx
+++ blueterra/components/cruise/BannerSection.jsx
@@ -43,7 +43,7 @@ function BannerSection({isMobile}) {
             <div className=" w-full h-auto absolute bg-black/10  inset-0 flex-center flex-col text-white ">
                 <div className="flex-center flex-col space-y-5 lg:space-y-8 mt-10 overflow-x-hidden ">
                     <h1 className={` ${playfair.className}  ${isBannerVideoLoaded ? 'opacity-100 translate-y-0' : ' translate-y-5 opacity-0'} translate-all duration-700 ease-in-out text-3xl  max-md:px-5 text-center md:text-4xl lg:text-[60px] xl:text-[70px] 2xl:text-[80px] font-semibold `}>BlueTerra Cruise</h1>
-                    <div className=" max-w-11/12     max-lg:max-h-[80vh]  max-lg:h-full  overflow-y-auto lg:max-w-10/12 sm:p-2 rounded-2xl "
+                    <div className=" max-w-11/12   border no-scroll-bar   max-lg:max-h-[80vh]  max-lg:h-full overflow-y-auto lg:max-w-10/12 sm:p-2 rounded-2xl "
                       style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }} data-lenis-prevent >
                         <CruiseWidget />
                     </div>
