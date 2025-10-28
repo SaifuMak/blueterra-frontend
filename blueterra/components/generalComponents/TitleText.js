597b39c8ad51dc752b44a3ef93bc4e6eac60438b
richurichu <paulsonrichu@gmail.com>
2025-10-28 13:26:20 +0530

    html tags change in our-story page

--- blueterra/components/generalComponents/TitleText.js
+++ blueterra/components/generalComponents/TitleText.js
@@ -2,7 +2,7 @@ import { playfair} from '@/app/fonts'
 
 export default function TitleText({className=' font-medium', text=''}){
     return(
-    <h1 className={`${playfair.className} ${className} text-dark-4B text-3xl  lg:text-4xl xl:text-5xl 2xl:text-[50px]`} >{text}</h1>
+    <h2 className={`${playfair.className} ${className} text-dark-4B text-3xl  lg:text-4xl xl:text-5xl 2xl:text-[50px]`} >{text}</h2>
         
     )
 }
