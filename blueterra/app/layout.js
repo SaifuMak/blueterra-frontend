6388165db8b473994fc928b094cd22441c412768
richurichu <paulsonrichu@gmail.com>
2025-10-28 16:05:41 +0530

    new test

--- blueterra/app/layout.js
+++ blueterra/app/layout.js
@@ -5,13 +5,19 @@ import { Toaster } from 'sonner';
 export const metadata = {
   title: "BlueTerra | Curated Travel and Experiences",
   description: "Discover Blueterra — your gateway to curated travel itineraries, unforgettable adventures, and handpicked travel packages across the globe.",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <body className={jost.className}> */}
+       <head>
+        {/* 👇 This is the important part */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body className="">
         <Toaster position="bottom-right" richColors />
 
